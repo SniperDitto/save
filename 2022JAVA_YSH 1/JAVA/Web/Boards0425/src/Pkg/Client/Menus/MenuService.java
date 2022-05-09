dc9f5aa45924a8862fadb59c5d45380a31e11aa3
SniperDitto <baesuk105@naver.com>
2022-05-09 17:31:07 +0900

    0509수업

--- 2022JAVA_YSH 1/JAVA/Web/Boards0425/src/Pkg/Client/Menus/MenuService.java
+++ 2022JAVA_YSH 1/JAVA/Web/Boards0425/src/Pkg/Client/Menus/MenuService.java
@@ -58,5 +58,31 @@ public class MenuService {
 		return result;
 	}
 	
+	public ArrayList<LmenuVO> getLmenusAll(String hMenuID){
+		ArrayList<LmenuVO> result = new ArrayList<LmenuVO>();
+		
+		try {
+			String sql = "{call PKG_MENUS.PROC_SEL_LMENUS_ALL(?,?)}";
+			ArrayList<String> params = new ArrayList<String>();
+			params.add(hMenuID);
+			
+			ResultSet rs = DBConn.select(sql, params, true);
+			
+			while(rs.next()) {
+				LmenuVO l = new LmenuVO();
+				l.setlMenuID(rs.getString("LMENUID"));
+				l.setlMenuName(rs.getString("LMENUNAME"));
+				l.setlMenuURL(rs.getString("LMENUURL"));
+				l.setMenuID(rs.getString("MENUID"));
+				result.add(l);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+	
 	
 }
