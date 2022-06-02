db18d787d35042d83c878e8df39a4afce8194cef
SniperDitto <baesuk105@naver.com>
2022-06-02 17:19:30 +0900

    0602수업

---
+++ 2022JAVA_YSH 1/C++/test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "유나바보";
+    return 0;
+}
---
+++ 2022JAVA_YSH 1/JAVA/SpringBoard/SpringBoard/src/main/java/pkg/admin/Service/AuthMemberServiceImpl.java
@@ -0,0 +1,24 @@
+package pkg.admin.Service;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import pkg.admin.DAO.AuthMemberDAO;
+import pkg.admin.DTO.AuthMemberDTO;
+
+@Service
+public class AuthMemberServiceImpl implements AuthMemberService{
+	
+	@Autowired
+	private AuthMemberDAO authMemberDAO;
+	
+	@Override
+	public List<AuthMemberDTO> getAuthMemberList(String searchText) {
+		List<AuthMemberDTO> authMemberList = authMemberDAO.getAuthMemberList(searchText);
+		return authMemberList;
+	}
+	
+}
