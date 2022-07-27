da70054ab2b5398ba02493fd9d200155d42a569b
SniperDitto <sniperditto@kakao.com>
2022-07-27 22:29:05 +0900

    0727

--- IdeaProjects/ProblemSolving/src/pkg/Class2/Josephus.java
+++ IdeaProjects/ProblemSolving/src/pkg/Class2/Josephus.java
@@ -1,6 +1,8 @@
 package pkg.Class2;
 
 import java.io.*;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 //https://www.acmicpc.net/problem/11866
@@ -9,108 +11,42 @@ public class Josephus {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		int[] delNumArr = new int[N];
+		int N = Integer.parseInt(st.nextToken());//총인원수
+		int K = Integer.parseInt(st.nextToken());//제외간격
 		
-		JosephusList j = new JosephusList(N);
-		for(int i=2;i<=N;i++){
-			j.addNum(i);
+		bw.append("<");
+		Deque<Integer> d = new LinkedList<>();
+		
+		int limit = N/K;
+		for(int i=1;i<=N;i++){
+			if(i%K==0){
+				bw.append(String.valueOf(i));
+				bw.append(", ");
+			}else{
+				d.add(i);
+			}
 		}
 		
-		int cnt = 0;
-		while (cnt<j.getSize()){
-			delNumArr[cnt] = j.delNum(K);
-			cnt++;
+		int count = 0;
+		while(d.size()>0){
+			int i = d.poll();
+			count++;
+			if(count==K){
+				bw.append(String.valueOf(i));
+				if(d.size()!=1){
+					bw.append(", ");
+				}
+				count=0;
+			}else{
+				d.add(i);
+			}
 		}
 		
+		bw.append(">");
 		bw.flush();
 		bw.close();
 		br.close();
 	}
 }
 
-class JosephusList{
-	private JosephusNum start;
-	private JosephusNum nextNum;
-	private int size;
-	
-	public JosephusList(int size){
-		this.start = new JosephusNum(1);
-		this.nextNum = this.start;
-		this.size = size;
-	}
-	
-	public void addNum(int value){
-		JosephusNum newNum = new JosephusNum(value);
-		
-		this.nextNum.setNext(newNum);
-		newNum.setPrev(this.nextNum);
-		this.nextNum = newNum;
-		
-		if(value==size){
-			this.start.setPrev(newNum);
-		}
-	}
-	
-	public boolean isEmpty(){
-		int tempSize = 1;
-		while()
-	}
-	
-	public int delNum(int count){
-		int tempCount = 0;
-		JosephusNum tempNum = this.start;
-		while(!isEmpty()){
-			tempCount++;
-			if(tempCount==count){
-				JosephusNum prev = tempNum.getPrev();
-				JosephusNum next = tempNum.getNext();
-				prev.setNext(next);
-				next.setPrev(prev);
-				this.start = next;
-				break;
-			}else{
-				tempNum = tempNum.getNext();
-			}
-		}
-		
-		return tempNum.getVal();
-	}
-	
-	public int getSize() {
-		return size;
-	}
-}
 
-class JosephusNum{
-	private int val;
-	private JosephusNum prev;
-	private JosephusNum next;
-	
-	public JosephusNum(int val){
-		this.val = val;
-		this.prev = null;
-		this.next = null;
-	}
-	
-	public int getVal() {
-		return val;
-	}
-	
-	public JosephusNum getPrev() {
-		return prev;
-	}
-	
-	public void setPrev(JosephusNum prev) {
-		this.prev = prev;
-	}
-	
-	public JosephusNum getNext() {
-		return next;
-	}
-	
-	public void setNext(JosephusNum next) {
-		this.next = next;
-	}
-}
